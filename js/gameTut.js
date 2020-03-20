f6484c7858ea66ffe861fe2acd01f3a454c28dfd
cameron1m <45009513+cameron1m@users.noreply.github.com>
2020-03-19 18:55:07 -0700

    intro to objects

--- js/gameTut.js
+++ js/gameTut.js
@@ -5,7 +5,7 @@ ctx.font = '30px Arial';
  
 var HEIGHT = 500;
 var WIDTH = 500;
-var FRAMERATE = 30;
+var FRAMETIME = 33;
 //enemy
 var enemy = {
     x:50,
@@ -26,36 +26,25 @@ var player = {
 
 
 
-setInterval(playerUpdate, FRAMERATE);
-setInterval(enemyUpdate, FRAMERATE);
+setInterval(update, FRAMETIME);
 
-
-
-function playerUpdate(){
-        player.x += player.spdX;
-        player.y += player.spdY;
-        ctx.fillText(player.name, player.x, player.y);
-        console.log('hello', player.x);
-       
-        if(player.x >= WIDTH || player.x <= 0){
-            player.spdX *= -1;
-        }
-        else if(player.y >= WIDTH || player.y <= 0){
-            player.spdY *= -1;
-        }
-       
+function update(){
+    entityUpdate(player);
+    entityUpdate(enemy);    
 }
-function enemyUpdate(){
-    enemy.x += enemy.spdX;
-    enemy.y += enemy.spdY;
-    ctx.fillText(enemy.name, enemy.x, enemy.y);
-    console.log('hello', enemy.x);
-   
-    if(enemy.x >= WIDTH || enemy.x <= 0){
-        enemy.spdX *= -1;
-    }
-    else if(enemy.y >= WIDTH || enemy.y <= 0){
-        enemy.spdY *= -1;
-    }
-   
+
+
+function entityUpdate(entity){
+    entity.x += entity.spdX;
+    entity.y += entity.spdY;
+        ctx.fillText(entity.name, entity.x, entity.y);
+        console.log('hello', entity.x);
+       
+        if(entity.x >= WIDTH || entity.x <= 0){
+            entity.spdX *= -1;
+        }
+        else if(entity.y >= WIDTH || entity.y <= 0){
+            entity.spdY *= -1;
+        }
+       
 }
