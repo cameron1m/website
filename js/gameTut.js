d4d5b7a54c39353cf0b7ab8f4049896196dd7519
cameron1m <45009513+cameron1m@users.noreply.github.com>
2020-04-01 14:55:37 -0700

    working on player controls, added self updates

--- js/gameTut.js
+++ js/gameTut.js
@@ -4,40 +4,85 @@ ctx.font = '30px Arial';
 var HEIGHT = 500;
 var WIDTH = 500;
 var FRAMETIME = 33;
-//enemy
-var enemy = {
-    x:50,
-    y:40,
-    spdX:30,
-    spdY:5,
-    name:"z",
-};
 
+var ballList = [];
+//Ball
+function Ball(id,x,y,spdX,spdY,name){
+  var self = {
+    id:id,
+    x:x,
+    y:y,
+    spdX:spdX,
+    spdY:spdY,
+    name:name,
+  };
+  ballList[id] = self;
+  self.updatePos = function(){
+    self.x += self.spdX;
+    self.y += self.spdY;
+      if(self.x >= WIDTH || self.x <= 0){
+        self.spdX *= -1;
+      }
+      else if(self.y >= WIDTH || self.y <= 0){
+        self.spdY *= -1;
+      }
+     
+  }
+  return self;
+}
+var playerList = [];
 //player
-var player = {
-    x:200,
-    y:30,
-    spdX:30,
-    spdY:5,
-    name:"a",
-};
+function Player(id,x,y,spdX,spdY,upPressed,downPressed,name){
+    var self = {
+        id:id,
+        x:x,
+        y:y,
+        spdX:spdX,
+        spdY:spdY,
+        upPressed:upPressed,
+        downPressed:downPressed,
+        name:name,
+    };
+    playerList[id] = self;
+    self.updatePos = function(){
+      
+      if(self.upPressed) self.y -= self.spdY;
+      if(self.downPressed) self.y += self.spdY;
 
-
+      if(self.y >= HEIGHT) self.y -= self.spdY;
+        
+      if(self.y <= 0) self.y += self.spdY;
+      
+    }
+    return self;
+}
+Ball('B1',250,250,20,5,'Ball');
+Player('P1',30,250,0,10,up1Pressed,down1Pressed,"Player 1");
+Player('P2',470,250,0,10,up2Pressed,down2Pressed,"Player 2");
 
 setInterval(update, FRAMETIME);
 
 function update(){
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    entityUpdate(player);
-    entityUpdate(enemy);    
+  ctx.clearRect(0,0,500,500);
+    for(var id in playerList){
+      var Player = playerList[id];
+        Player.updatePos();
+        ctx.fillText(Player.name, Player.x, Player.y);
+    }
+    for(var id in ballList){
+      var Ball = ballList[id];
+   // Ball.updatePos();
+    ctx.fillText(Ball.name, Ball.x, Ball.y);
+    }
+    
 }
 
-
+///CURRENTLY UNUSED
 function entityUpdate(entity){
-    entity.x += entity.spdX;
-    entity.y += entity.spdY;
+      entity.x += entity.spdX;
+      entity.y += entity.spdY;
         ctx.fillText(entity.name, entity.x, entity.y);
-        console.log('hello', entity.x);
+        // console.log('hello', entity.x);
        
         if(entity.x >= WIDTH || entity.x <= 0){
             entity.spdX *= -1;
@@ -47,3 +92,54 @@ function entityUpdate(entity){
         }
        
 }
+
+//USER INPUT
+var up1Pressed = false;
+var up2Pressed = false;
+var down1Pressed = false;
+var down2Pressed = false;
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+  //checks if key is down
+  function keyDownHandler(event) {
+  
+      if(event.keyCode == 83) {
+          down1Pressed = true;
+        console.log("s is down");
+      }
+      else if(event.keyCode == 87) {
+        up1Pressed = true;
+        console.log("w is down");
+      }
+  
+      if(event.keyCode == 40) {
+        down2Pressed = true;
+        console.log("down arrow is down");
+      }
+      else if(event.keyCode == 38) {
+        up2Pressed = true;
+        console.log("up arrow is down");
+      }
+  }
+  // checks if key is up
+    function keyUpHandler(event) {
+  
+      if(event.keyCode == 83) {
+          down1Pressed = false;
+       // console.log("s is up");
+      }
+      else if(event.keyCode == 87) {
+        up1Pressed = false;
+        //console.log("w is up");
+      }
+  
+      if(event.keyCode == 40) {
+        down2Pressed = false;
+        //console.log("down arrow is up");
+      }
+      else if(event.keyCode == 38) {
+        up2Pressed = false;
+        //console.log("up arrow is up");
+      }
+  }
